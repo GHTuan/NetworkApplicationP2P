65b5758228e3cae0ad6724a281982b6321503f25
Tuan <pmtuan.tp@gmail.com>
2023-11-13 14:09:03 +0700

    adding all files

---
+++ Client.py
@@ -0,0 +1,57 @@
+import socket
+import os
+
+HEADER = 64
+FORMAT = 'utf-8'
+
+HOST = "26.75.111.47"
+PORT = 5554
+
+DISCONECT = "DISCONECT"
+TRANSFER = "TRANSFER FILE"
+
+
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST,PORT))
+
+
+def send_FORMAT(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    
+
+def send_file():
+    
+    file_name = input("File Name: ")
+    file_size = os.path.getsize("./send/"+file_name)
+    
+    send_FORMAT(TRANSFER)
+    
+    send_FORMAT(file_name)
+    
+    send_FORMAT(str(file_size))
+    
+    with open("./send/"+file_name,"rb") as file:
+        c=0
+        while c <= file_size:
+            data = file.read(1024)
+            if not (data):
+                break
+            client.sendall(data)
+            c+=len(data)
+    print("Transfer Complete")
+    
+    
+def disconect():
+    client.send(DISCONECT.encode())
+    print(f"DISCONECT")
+    client.close()
+    
+    
+send_file()
+#disconect()
---
+++ Server.py
@@ -0,0 +1,67 @@
+import socket
+import threading
+
+HEADER = 64
+FORMAT = 'utf-8'
+
+
+HOST = socket.gethostbyname(socket.gethostname())
+PORT = 5554
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST,PORT))
+
+
+DISCONECT = "DISCONECT"
+TRANSFER = "TRANSFER FILE"
+
+
+def recv_FORMAT(conn):
+    msg_length = conn.recv(HEADER).decode(FORMAT)
+    if msg_length:
+        msg_length = int(msg_length)
+        return conn.recv(msg_length).decode(FORMAT)
+    pass
+    
+
+def handle_client(conn,addr):
+    print (f"NEW CONNECTION {addr} connected.")
+    connected = True
+    while connected: 
+        CODE = recv_FORMAT(conn)
+        if CODE==TRANSFER:
+            file_name = recv_FORMAT(conn)
+            print(f"{file_name}")
+            file_size = recv_FORMAT(conn)
+            print(f"{file_size}")
+            with open("./recive/"+file_name,"wb") as file:
+                c=0
+                while c <= int(file_size):
+                    data = conn.recv(1024)
+                    if not (data):
+                        break
+                    file.write(data)
+                    c+=len(data)
+                print("Transfer Complete")
+        elif CODE == DISCONECT:
+            print(f"[{addr}] DISCONECTED")
+            connected=False
+    conn.close()
+                  
+def start():
+    server.listen()
+    print(f"Listening at address {HOST}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn,addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTONS] {threading.active_count() - 1}")
+
+
+print("Starting the server")
+start()
+
+
+
+   
+    
