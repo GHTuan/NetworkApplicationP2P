ed487c113d6b5cfe404a97b2800eb66cc3d96249
Tuan <pmtuan.tp@gmail.com>
2023-11-14 22:32:16 +0700

    Add Peer.py and its task

---
+++ Peer.py
@@ -0,0 +1,4 @@
+#Establishes the connecting to the main server (hard code the HOST and PORT) (reference: Sender.py) 
+#After establish the connection to the main server setup your own server using your local IP and on port 43432.
+#Requrement: Your connection to the main server and your own server must run on diffent thread
+#            The server must be able to handle multiple connecting (reference: Receive.py) 
